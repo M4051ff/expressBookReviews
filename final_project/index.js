4eb4226e0a8785433a4e2fc6c7bfa9c475ba821c
M4051ff <bussingtont@gmail.com>
2023-09-18 12:28:50 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -5,14 +5,27 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    //Write the authenication mechanism here
+    
+      // Check for access token in session
+      if(req.session.accessToken) {
+    
+        // Pass request to next middleware
+        next();
+    
+      } else {
+    
+        // Return unauthorized if no token
+        res.status(401).send('Unauthorized');
+    
+      }
+    
+    });
  
 const PORT =5000;
 
